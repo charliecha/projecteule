df95cdecc8b844a4b0cd5172c75d6f8aa2437f8e
charliecha <zhazongxun@gmail.com>
2015-06-23 17:11:18 +0800

    add Problems 57, 58

---
+++ src/bean17/cha/Problem57.java
@@ -0,0 +1,109 @@
+package bean17.cha;
+
+import java.math.BigInteger;
+
+/**
+ * Square root convergents Problem 57 It is possible to show that the square
+ * root of two can be expressed as an infinite continued fraction.
+ * 
+ * ¡Ì 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+ * 
+ * By expanding this for the first four iterations, we get:
+ * 
+ * 1 + 1/2 = 3/2 = 1.5 1 + 1/(2 + 1/2) = 7/5 = 1.4 1 + 1/(2 + 1/(2 + 1/2)) =
+ * 17/12 = 1.41666... 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+ * 
+ * The next three expansions are 99/70, 239/169, and 577/408, but the eighth
+ * expansion, 1393/985, is the first example where the number of digits in the
+ * numerator exceeds the number of digits in the denominator.
+ * 
+ * In the first one-thousand expansions, how many fractions contain a numerator
+ * with more digits than denominator?
+ * 
+ * @author bean17.cha
+ */
+public class Problem57 {
+	final int N = 1000;
+
+	/**
+	 * @param args
+	 *            arguments
+	 */
+	public static void main(String[] args) {
+		new Problem57().testLychrelNumbers();
+	}
+
+	private void testLychrelNumbers() {
+		long start = System.currentTimeMillis();
+
+		int c = 0;
+
+		Fraction oldFraction = new Fraction(BigInteger.ONE, BigInteger.ONE);
+		Fraction newFraction;
+		for (int i = 0; i < N; i++) {
+			newFraction = oldFraction.add(1).reverse().add(1);
+			oldFraction = newFraction;
+
+			int numerator = String.valueOf(newFraction.numerator).length();
+			int denominator = String.valueOf(newFraction.denominator).length();
+			if (numerator > denominator) {
+				c++;
+			}
+
+			// System.out.println(newFraction);
+		}
+
+		System.out.println("c = " + c);
+		System.out.println("cost time : "
+				+ (System.currentTimeMillis() - start) + "ms.");
+	}
+
+	double sqrt(final int n) {
+		double old = 1;
+		double new1 = 1 + (n - 1) / (1 + old);
+		double interval = 0.000001;
+		while (Double.compare(Math.abs(old - new1), interval) >= 0) {
+			old = new1;
+			new1 = 1 + (n - 1) / (1 + old);
+		}
+		return new1;
+	}
+
+	class Fraction {
+		public Fraction(BigInteger numerator, BigInteger denominator) {
+			super();
+			this.numerator = numerator;
+			this.denominator = denominator;
+		}
+
+		BigInteger numerator;
+		BigInteger denominator;
+
+		Fraction add(int n) {
+			// numerator += denominator * n;
+			numerator = numerator.add(denominator.multiply(BigInteger
+					.valueOf(n)));
+			return this;
+		}
+
+		Fraction mutiply(int n) {
+			// numerator *= n;
+			numerator = numerator.multiply(BigInteger.valueOf(n));
+			return this;
+		}
+
+		Fraction reverse() {
+			BigInteger t = numerator;
+			numerator = denominator;
+			denominator = t;
+			return this;
+		}
+
+		@Override
+		public String toString() {
+			return "Fraction [numerator=" + numerator + ", denominator="
+					+ denominator + "]";
+		}
+
+	}
+}
---
+++ src/bean17/cha/Problem58.java
@@ -0,0 +1,56 @@
+package bean17.cha;
+
+/**
+ * 
+ Spiral primes Problem 58 Starting with 1 and spiralling anticlockwise in the
+ * following way, a square spiral with side length 7 is formed.
+ * 
+ * 37 36 35 34 33 32 31 38 17 16 15 14 13 30 39 18 5 4 3 12 29 40 19 6 1 2 11 28
+ * 41 20 7 8 9 10 27 42 21 22 23 24 25 26 43 44 45 46 47 48 49
+ * 
+ * It is interesting to note that the odd squares lie along the bottom right
+ * diagonal, but what is more interesting is that 8 out of the 13 numbers lying
+ * along both diagonals are prime; that is, a ratio of 8/13 ¡Ö 62%.
+ * 
+ * If one complete new layer is wrapped around the spiral above, a square spiral
+ * with side length 9 will be formed. If this process is continued, what is the
+ * side length of the square spiral for which the ratio of primes along both
+ * diagonals first falls below 10%?
+ * 
+ */
+public class Problem58 {
+	int primes = 0;
+	int totols = 0;
+	double target = 0.1D;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Problem58().testSpiralPrimes();
+	}
+
+	private void testSpiralPrimes() {
+		long start = System.currentTimeMillis();
+
+		int radius = 0;
+		int current = 1;
+		totols++;
+		double radio = 1D * primes / totols;
+		while (Double.compare(radio, target) >= 0 || primes == 0) {
+			radius++;
+			for (int i = 0; i < 4; i++) {
+				current += radius << 1;
+				if (Utils.isPrime(current)) {
+					primes++;
+				}
+				totols++;
+			}
+			radio = 1D * primes / totols;
+		}
+		System.out.println("square =  " + (radius * 2 + 1));
+
+		System.out.println("cost time : "
+				+ (System.currentTimeMillis() - start) + "ms.");
+	}
+}

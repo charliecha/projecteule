2a475544c1f9685242c8350bf0623c7219a0c721
Cha <bean17.cha@gmail.com>
2015-09-14 00:00:55 +0800

    update Problem 31

--- src/bean17/cha/Problem31.java
+++ src/bean17/cha/Problem31.java
@@ -22,14 +22,20 @@ public class Problem31 {
 				+ (System.currentTimeMillis() - start) + "ms.");
 	}
 
-	private int coin(int n, int goal) {
-		if (1 == n || 0 == goal) {
+	/**
+	 * 硬币之和
+	 * @param index 索引值
+	 * @param goal 目标值
+	 * @return 硬币组合之和
+	 */
+	private int coin(int index, int goal) {
+		if (1 == index || 0 == goal) {
 			return 1;
 		} else {
-			final int c = goal / units[n - 1];
+			final int c = goal / units[index - 1];
 			int result = 0;
 			for (int i = 0; i <= c; i++) {
-				result += coin(n - 1, goal - units[n - 1] * i);
+				result += coin(index - 1, goal - units[index - 1] * i);
 			}
 			return result;
 		}
